3b7db6cfa70f59ad8213d1db88701f32902ac86a
p4ffos <danilbaranov58@gmail.com>
2023-10-24 14:53:17 +0200

    Initial commit

---
+++ src/StudentScores/IOScanner.java
@@ -0,0 +1,19 @@
+package StudentScores;
+
+import java.util.Scanner;
+public class IOScanner {
+    static Scanner scanner = new Scanner(System.in);
+    static int readIntWithMessage(String messageFromString) {
+        System.out.println(messageFromString);
+        int intNumber = scanner.nextInt();
+        scanner.nextLine();
+        return intNumber;
+    }
+    static String readStringWithMessage(String messageFromString) {
+        System.out.println(messageFromString);
+        return scanner.nextLine();
+    }
+    static void close() {
+        scanner.close();
+    }
+}
